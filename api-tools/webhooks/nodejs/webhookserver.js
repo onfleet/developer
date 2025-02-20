795fbf7c2ab6700f684555dfc05331fb92b189b1
Shuai Tseng <shuai@onfleet.com>
2025-02-19 17:06:17 -0800

    added changes from review

--- api-tools/webhooks/nodejs/webhookserver.js
+++ api-tools/webhooks/nodejs/webhookserver.js
@@ -2,14 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser')
-
+const jsonParser = bodyParser.json()
 //const fs = require('node:fs');
-
-var jsonParser = bodyParser.json()
-
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
 
 //GET method query validation
 app.get("/webhooks", (req, res) => {
@@ -17,19 +11,24 @@ app.get("/webhooks", (req, res) => {
   //console.log(req.query.check);
 })
 
+//POST method for the webhook payload
 app.post("/webhooks", jsonParser, (req, res) => {
   res.status(200).send(req.query.check);
   var content = JSON.stringify(req.body.data) + '';
-  console.log(content); //the webhook payload
+  console.log(content)
+}) 
 
-  /*  Only if you want to write it to a local File Directory
-  //  Set the [Path]    
-  fs.writeFileSync('[path]', content, err => {
-    if (err) {
-      console.error(err);
-      console.log(content);
-    } else {
-      // file written successfully
-    }
-  });*/
-})
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+/*  Only if you want to write it to a local File Directory
+//  Set the [Path]    
+fs.writeFileSync('[path]', content, err => {
+  if (err) {
+    console.error(err);
+    console.log(content);
+  } else {
+    // file written successfully
+  }
+});*/
