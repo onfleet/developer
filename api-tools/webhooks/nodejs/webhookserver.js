5cc0d362fe982fc644b2e804d0734dbe6adbe14f
Shuai Tseng <62573713+shuaitseng@users.noreply.github.com>
2025-02-21 17:53:09 -0800

    add nodejs server and python in its own folder (#27)
    * add nodejs server and python in its own folder
    
    * added changes from review
    
    * Update webhookserver.js
    
    use req.body instead of req.body.data

---
+++ api-tools/webhooks/nodejs/webhookserver.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json()
+//const fs = require('node:fs');
+
+//GET method query validation
+app.get("/webhooks", (req, res) => {
+  res.status(200).send(req.query.check);
+  //console.log(req.query.check);
+})
+
+//POST method for the webhook payload
+app.post("/webhooks", jsonParser, (req, res) => {
+  res.status(200).send(req.query.check);
+  var content = JSON.stringify(req.body) + '';
+  console.log(content)
+}) 
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+/*  Only if you want to write it to a local File Directory
+//  Set the [Path]    
+fs.writeFileSync('[path]', content, err => {
+  if (err) {
+    console.error(err);
+    console.log(content);
+  } else {
+    // file written successfully
+  }
+});*/
